c019451c248dca38ff1c8f9a3da54bc89ef145ee
Семеняко Диана Дмитриевна <diana.semenyako@gmail.com>
2026-02-22 18:54:07 +0300

    Update task_2-2_inventory_control.py

--- projects_2/task_2_2/task_2-2_inventory_control.py
+++ projects_2/task_2_2/task_2-2_inventory_control.py
@@ -1,5 +1,10 @@
 new_reagent = input('Введите название нового реагента: ')
 clear_reagent = new_reagent.upper()
 quantity_of_reagent = input('Введите количество реагента: ')
+result = (f'Реактив {clear_reagent} поступил на склад в количестве {quantity_of_reagent} шт.')
 
-print(f'Реактив {clear_reagent} поступил на склад в количестве {quantity_of_reagent} шт.')
+print(result)
+
+f = open("C:\\Users\\User\\Documents\\semenyako_dd\\projects_2\\task_2_2\\inventory.txt", 'w', encoding="utf-8")
+print(result, file=f)
+f.close()
